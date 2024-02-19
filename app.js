c89f8b62b2336335baf2c247a81728210f32f356
Amna Kafeel <160414049+AmnaKafeel@users.noreply.github.com>
2024-02-19 12:29:26 +0500

    Personal Message:

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 "use strict";
-let massage = "Hello World";
-console.log(massage);
+let personName = '';
+personName = prompt('what is your name?') || '';
+if (pesonName !== null && parsonName !== '') {
+    alert(`Hello ${personName}, would you like to learn some Python today?`);
+}
+else {
+    alert('you have to fill your name');
+}
--- app.ts
+++ app.ts
@@ -1,2 +1,7 @@
-let massage= "Hello World"
-console.log("massage")
+let personName :string = '';
+personName = prompt('what is your name?') ||'';
+if(pesonName !==null && parsonName !==''){
+alert(`Hello ${personName}, would you like to learn some Python today?`) }
+else{
+    alert('you have to fill your name')
+}
